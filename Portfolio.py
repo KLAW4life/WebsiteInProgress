8c1169427829c705f772b66cd98c26b77b019f27
KLAW4life <kerene789@gmail.com>
2024-10-10 00:34:17 -0400

    Created Homepage and Added Typing Animation

--- Portfolio.py
+++ Portfolio.py
@@ -3,6 +3,10 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 
 Portfolio = Flask(__name__)
 
+@Portfolio.route('/')
+def index():
+    return render_template('homepage.html')
+
 
 if __name__ == '__main__':
     Portfolio.run(debug=True)
